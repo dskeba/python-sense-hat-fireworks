7ccb1c3697489d34584fbb0b0f0495abb77c7f3d
Derek Skeba <dskeba@dskeba.com>
2016-03-23 03:11:24 +0000

    Fix bugs

--- fireworks.py
+++ fireworks.py
@@ -15,48 +15,50 @@ class Firework():
 		self.explode = False
 		
 	def animate(self):
-		if self.pos[1] < 5:
+		if self.pos[1] < 4:
 			self.pos[0] = self.pos[0] + self.horz_speed
 			self.pos[1] = self.pos[1] + self.vert_speed
 		else:
-			if !self.explode:
-				self.burst = Burst(self.pos)
+			if not self.explode:
+				self.burst = Burst(self.pos, self.color)
 				self.explode = True
 			self.burst.animate()
 		
 	def render(self, sensehat):
-		if !self.explode:
+		if not self.explode:
 			sensehat.set_pixel(self.pos[0], self.pos[1], self.color[0], self.color[1], self.color[2])
 		else:
-			self.burst.render()
+			self.burst.render(sensehat)
 
 class Burst():
 
-	def __init__(self):
-		self.pos = [0,0]
-		self.color = [0,0,0]
+	def __init__(self, pos, color):
+		self.pos = pos
+		self.color = color
 		self.vert_speed = 0
 		self.horz_speed = 0
 		self.particle_count = 5;
+		self.particle = []
 		self.create_particles();
 		
 	def create_particles(self):
-		for i in range(0, particle_count):
-			self.particle[i] = Particle(self.pos)
+		for i in range(0, self.particle_count):
+			self.particle.append(Particle(self.pos, self.color))
 		
 	def animate(self):
-		for i in range(0, particle_count):
+		for i in range(0, self.particle_count):
 			self.particle[i].animate()
 			
 	def render(self, sensehat):
-		for i in range(0, particle_count):
+		for i in range(0, self.particle_count):
 			self.particle[i].render(sensehat)
+			print(self.particle[i].pos[0])
 		
 class Particle():
 
-	def __init__(self, pos):
+	def __init__(self, pos, color):
 		self.pos = pos
-		self.color = [0,0,0]
+		self.color = color
 		self.vert_speed = random.randint(-1,1)
 		self.horz_speed = random.randint(-1,1)
 		
@@ -70,9 +72,10 @@ class Particle():
 		sensehat.set_pixel(self.pos[0], self.pos[1], self.color[0], self.color[1], self.color[2])
 		
 fw = Firework()	
+time.sleep(0.5)
 while True:
 	sense.clear()
 	fw.animate()
-	fw.render(sensehat)
-	sense.set_rotation(180)
-	
+	fw.render(sense)
+
+	time.sleep(0.5)
